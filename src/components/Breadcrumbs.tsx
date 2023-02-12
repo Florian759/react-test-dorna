4acdff17ed1410bb27960109a5c5ea769606bd9d
Florian Gonzalez <floriangonz@gmail.com>
2023-02-13 00:14:16 +0100

    feat: Create Breadcrumbs component

---
+++ src/components/Breadcrumbs.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Breadcrumbs as BreadcrumbsMUI } from '@mui/material';
+import { useNavigate } from 'react-router';
+import { BreadcrumbHover, WrapperBreadCrumbs } from './style/BreadcrumbsStyle';
+
+export type PropsTypeBread = {
+	name: string | undefined;
+	to: string | undefined;
+}
+
+type PropsType = {
+	data?: Array<PropsTypeBread>;
+}
+
+export function Breadcrumbs({data}: PropsType): JSX.Element {
+  const navigate = useNavigate();
+
+  const handleOnClick = (to: string | undefined) => {
+    if (to) navigate(to);
+  }
+
+  return (
+    <WrapperBreadCrumbs>
+			<BreadcrumbsMUI aria-label="breadcrumb">
+				{data?.map((item, index) => <BreadcrumbHover
+					key={`${item?.name}${index}` }
+					label={item?.name}
+					onClick={() => handleOnClick(item.to)}
+				/>)}
+			</BreadcrumbsMUI>
+    </WrapperBreadCrumbs>   
+  )
+}
+
+export default Breadcrumbs;
---
+++ src/components/style/BreadcrumbsStyle.tsx
@@ -0,0 +1,35 @@
+import { emphasize, styled } from '@mui/material/styles';
+import Chip from '@mui/material/Chip';
+
+export const WrapperBreadCrumbs = styled('div')(`
+  width: 100%;
+  padding: 10px 0px;
+`);
+
+export const BreadcrumbHover = styled(Chip)(({ theme }) => {
+    const backgroundColor = theme.palette.mode === 'light'
+        ? theme.palette.grey[100]
+        : theme.palette.grey[800];
+  
+    return {
+      backgroundColor,
+      height: theme.spacing(3),
+      color: theme.palette.text.primary,
+      fontWeight: 500,
+      '& .MuiChip-icon': {
+        color: theme.palette.text.primary,
+      },
+      '&:hover, &:focus': {
+        color: theme.palette.text.secondary,
+        backgroundColor: emphasize(backgroundColor, 0.06),
+        '& .MuiChip-icon': {
+                color: theme.palette.text.secondary,
+        },
+      },
+      '&:active': {
+        boxShadow: theme.shadows[1],
+        backgroundColor: emphasize(backgroundColor, 0.12),
+      },
+    };
+  });
+  
