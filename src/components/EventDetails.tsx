c2ac96fbe47cbc22890faf3f09efac57f634d841
Florian Gonzalez <floriangonz@gmail.com>
2023-02-13 09:56:03 +0100

    feat: Create EventDetails component

---
+++ src/components/EventDetails.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+import { IEvent } from '../models/IEvent';
+import PropertyLabel from './PropertyLabel';
+import { WrapperCard } from './style/AppStyle';
+import { getCircuitUrl } from '../utils/getUrls';
+import { dateLongFormat } from '../utils/formatDate';
+import { CircuitImage, WrapperImage } from './style/EventItemStyle';
+
+export type PropsTypeEventCard = {
+  data?: IEvent;
+}
+
+export function EventDetails({data}: PropsTypeEventCard) {
+	const code: string | undefined = data?.code ;
+
+  return (
+    <WrapperCard >
+      <Grid container spacing={1}>
+
+        <Grid item xs={12} sm={12} md={4}>
+          <WrapperImage>
+            {code && <CircuitImage alt={code} src={getCircuitUrl(code)} />}
+          </WrapperImage>
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={8} >
+          <PropertyLabel title="Code:" value={code?.toLocaleUpperCase()} />
+          <PropertyLabel title="Short Name:" value={data?.shortName.toLocaleLowerCase()} />
+          <PropertyLabel title="Sponsored  Name:" value={data?.sponsoredName} />
+          <PropertyLabel title="Circuit  Name:" value={data?.circuit?.name} />
+          <PropertyLabel title="Circuit  Location:" value={data?.circuit?.location} />
+          <PropertyLabel title="Date of Start:" value={dateLongFormat(data?.schedule.localTime.dateOfStart)} />
+          <PropertyLabel title="Date of End:" value={dateLongFormat(data?.schedule.localTime.dateOfEnd)} />
+          <PropertyLabel title="Sequence:" value={data?.sequence.toString()} />
+        </Grid>
+
+      </Grid>
+    </WrapperCard>
+  )
+}
+
+export default EventDetails;
---
+++ src/components/style/EventDetailsStyle.tsx
@@ -0,0 +1,41 @@
+import { styled } from '@mui/material/styles';
+import { Typography } from '@mui/material';
+
+export const WrapperAbsolute = styled('div')(({ theme }) => `
+  z-index: 999;
+  position: absolute;
+  left: 15px;
+  top: 15px;
+`);
+
+export const WrapperTitleEvent = styled('div')(({ theme }) => `
+  width: 100%;
+  height: 44px;
+  padding: 0 10px;
+  border-radius: 10px;  
+  display: flex;
+  align-items: center;
+`);
+
+export const CodeEvent = styled(Typography)(({ theme }) => ({
+    flexGrow: 1,
+    fontWeight: 600,
+    fontSize: '1em',
+    textTransform: 'uppercase'
+}));
+  
+export const WrapperImage = styled('div')(({ theme }) => `
+  height: 100%;
+  width: 100%;
+  box-sizing: border-box;
+  display: inline-block;
+  position: relative;
+  // background-color: red;
+`);
+
+export const CircuitImage = styled('img')(({ theme }) => `
+  width: 100%;
+  height: 188px;
+  aspect-ratio: 1 / 1;
+  object-fit: contain;
+`);
