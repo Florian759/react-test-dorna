f562baf88a155a7369db109fc48d087162128454
Florian Gonzalez <floriangonz@gmail.com>
2023-02-13 03:04:58 +0100

    style: WrapperCard moves for general styles

--- src/components/style/AppStyle.tsx
+++ src/components/style/AppStyle.tsx
@@ -29,3 +29,15 @@ export const WrapperFlexRow = styled('div')(`
   display: flex;
   flex-direction: row;
 `);
+
+export const WrapperCard = styled('div')(({ theme }) => `
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 30px 15px;
+  border-radius: 10px;
+  border-top: 2px solid ${theme.palette.primary.main};
+  box-shadow: 0 2px 2px 0 rgba(0,0,0,.1);
+  background-color: rgba(255,255,255,.8);
+  box-sizing: border-box;
+`);
