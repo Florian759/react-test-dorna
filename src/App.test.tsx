de6c9234627a1a5cb4d270ca5d04cbe9c554a2c0
Florian Gonzalez <floriangonz@gmail.com>
2023-02-13 10:59:12 +0100

    test: Init jest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,27 @@
+/* eslint-disable testing-library/prefer-screen-queries */
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import { BrowserRouter } from 'react-router-dom'
 import App from './App';
+import { store } from './redux/store';
+import theme from './theme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders react test', () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <BrowserRouter >
+          <App />
+        </BrowserRouter >
+      </ThemeProvider>,
+    </Provider>
+  );
+
+  expect(getByText(/test dorna/i)).toBeInTheDocument();
 });
+
