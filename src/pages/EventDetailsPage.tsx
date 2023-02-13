dab1a428d22ca0283bf84c3ed484228a8d24d505
Florian Gonzalez <floriangonz@gmail.com>
2023-02-13 09:56:32 +0100

    feat: Create EventDetailsPage component

--- src/pages/EventDetailsPage.tsx
+++ src/pages/EventDetailsPage.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useAppSelector } from '../redux/hooks';
+import { useNavigate, useParams } from 'react-router-dom';
+import { selectSeason } from '../reducers/seasonSlice';
+import { WrapperRoot } from '../components/style/AppStyle';
+import Breadcrumbs, { PropsTypeBread } from '../components/Breadcrumbs';
+import { IEvent } from '../models/IEvent';
+import EventDetails from '../components/EventDetails';
 
-export function EventDetailsPage() {
+export function EventDetailsPage () {
+  const { id } = useParams();
+  const { events } = useAppSelector(selectSeason);
+  const [ data, setData ] = useState<IEvent>();
+  const navigate = useNavigate();
+
+  const breadcrumbs: Array<PropsTypeBread> = [
+		{name: "Season", to: "/"},
+    {name: data?.shortName, to: undefined},
+	]
+
+  useEffect(() => {
+    if (id) {
+      const found: IEvent | undefined = events.find(item => item.id === id);    
+      if (found) {
+        setData(found);
+      } else {
+        navigate(`/`);
+      }
+    }
+  }, [id, events, navigate]);
 
   return (
-    <div>
-        EventDetailsPage
-    </div>
+    <WrapperRoot>
+        <Breadcrumbs data={breadcrumbs}/>
+        <EventDetails data={data}/>
+    </WrapperRoot>
   )
 }
 
