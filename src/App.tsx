eebbfa9552edbac773b31835c4102aa588f7cb82
Florian Gonzalez <floriangonz@gmail.com>
2023-02-13 12:26:12 +0100

    feat: Create custom hook to fetch calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom'
 import EventDetailsPage from './pages/EventDetailsPage';
 import SeasonDetailsPage from './pages/SeasonDetailsPage';
-import { seasonSetData, selectSeason } from './reducers/seasonSlice';
-import { useAppDispatch, useAppSelector } from './redux/hooks';
-import { calendarService } from './services/calendarService';
-import { ICalendar } from './models/ICalendar';
 import { ContainerWithHeader, WrapperApp } from './components/style/AppStyle';
 import Header from './components/Header';
 import HelpPage from './pages/HelpPage';
+import { useCalendar } from './hooks/useCalendar';
 
 function App() {
-  const season = useAppSelector(selectSeason) ;
-  const dispatch = useAppDispatch();
-  
-  useEffect(() => {
-    if (!season.season?.id) {
-      calendarService().then(response => {
-        const seasons: Array<ICalendar> = response.data
-        dispatch(seasonSetData(seasons))
-      }).catch(e => {
-        console.log(e);
-      }) 
-    }
-  }, [season, dispatch]);
+  useCalendar();
 
   return (
     <WrapperApp>
---
+++ src/hooks/useCalendar.ts
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { ICalendar } from '../models/ICalendar';
+import { seasonSetData, selectSeason } from '../reducers/seasonSlice';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import { calendarService } from '../services/calendarService';
+
+export function useCalendar() {
+    const season = useAppSelector(selectSeason) ;
+    const dispatch = useAppDispatch();
+    
+    useEffect(() => {
+      if (!season.season?.id) {
+        calendarService().then(response => {
+          const seasons: Array<ICalendar> = response.data
+          dispatch(seasonSetData(seasons))
+        }).catch(e => {
+          console.log(e);
+        }) 
+      }
+    }, [season, dispatch])
+
+}
